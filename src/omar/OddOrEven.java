e05872057572ea3ae14dbdb454c8fa63dbf63f5b
ZarielSmite <omar.that.kick.tho@gmail.com>
2025-01-08 01:17:05 +0300

    Did The OddOrEven Tasl

---
+++ src/omar/OddOrEven.java
@@ -0,0 +1,20 @@
+package omar;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static void OddOrEven(int num){
+        if(num%2==0){
+            System.out.println("Even");
+        }else
+            System.out.println("Odd");
+
+
+    }
+
+}
