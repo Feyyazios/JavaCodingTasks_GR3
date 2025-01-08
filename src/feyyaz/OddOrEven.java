fba169ae565ec08207a08d89163e4882f5204ce5
HelloWorld <feyyazbogaakca@gmail.com>
2025-01-08 21:00:41 +0000

    Method has Created.

--- src/feyyaz/OddOrEven.java
+++ src/feyyaz/OddOrEven.java
@@ -2,9 +2,24 @@ package feyyaz;
 
 public class OddOrEven {
     /*
+**Numbers -- odd & even**
 
+       Write a method that can identify  a given number is even or odd.
+
+       EX:
+
+              identify(5) --> "Odd"
+              identify(6) --> "Even"
      */
     public static void main(String[] args) {
 
+    }
+
+    public static void oddOrEven(int num) {
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
     }
 }
