929d40c5da750e7082c25376d9cd11ec1841e049
HelloWorld <feyyazbogaakca@gmail.com>
2025-01-08 20:57:57 +0000

    Method has Created.

--- src/zack/OddOrEven.java
+++ src/zack/OddOrEven.java
@@ -6,4 +6,11 @@ public class OddOrEven {
 
 
     }
+    public static void oddOrEven(int num) {
+        if ( num % 2 == 0 ) {
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
 }
