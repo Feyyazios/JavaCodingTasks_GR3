9f3a5baeab3555c51658a7540a0db881fe9ec8f6
nurte <nurtenglryz@gmail.com>
2025-01-09 13:27:39 +0000

    OddOrEven created

---
+++ src/nurten/OddOrEven.java
@@ -0,0 +1,16 @@
+package nurten;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+
+        int number = 0;
+
+        if(number % 2 == 0){
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+
+    }
+}
