ee71985cc13bf56500038ba389adde91af75e0db
dilekdeniz <barisdeniz@hotmail.co.uk>
2025-01-09 11:52:38 +0000

    OddOrEven tasks done1

--- src/Baris/OddOrEven.java
+++ src/Baris/OddOrEven.java
@@ -2,5 +2,15 @@ package Baris;
 
 public class OddOrEven {
     public static void main(String[] args) {
+        OddOrEvenMethod(2);
+
     }
-}
+
+    public static void OddOrEvenMethod(int num) {
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else
+            System.out.println("Odd");
+
+    }
+    }
