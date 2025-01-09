4dd8d4dee9e62b044b10dec3b3833f5782f04e9c
nurte <nurtenglryz@gmail.com>
2025-01-09 13:38:44 +0000

    OddOrEven recreated

--- src/nurten/OddOrEven.java
+++ src/nurten/OddOrEven.java
@@ -3,7 +3,7 @@ package nurten;
 public class OddOrEven {
     public static void main(String[] args) {
 
-        int number = 0;
+        int number = 5;
 
         if(number % 2 == 0){
             System.out.println("Even");
