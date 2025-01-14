b05aba757b3a2635bc91a6444a61ac12d5129a77
abletcan <ablet0991@hotmail.com>
2025-01-14 09:59:29 +0100

    Ablet - OddOrEven file added.

---
+++ src/ablet/OddOrEven.java
@@ -0,0 +1,14 @@
+package ablet;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        int num = 11;
+
+        if(num % 2 == 0){
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+    }
+}
