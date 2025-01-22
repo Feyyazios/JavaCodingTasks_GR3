450f7a19a404c3642f9d3b8796ebaf0bd3dc7eca
selma <selmacaglar-32@gmail.com>
2025-01-22 01:32:27 +0000

    Bug fixed

--- src/selma/FINRA.java
+++ src/selma/FINRA.java
@@ -4,37 +4,34 @@ public class FINRA {
 
     public static void main(String[] args) {
 
-        finra(55);
-
+        finra(30);
 
     }
 
-    public static void finra(int number) {
+    public static  void finra (int number) {
 
-        if (number % 3 == 0 && number % 5 == 0) {
-            System.out.println("FINRA");
-        } else if (number % 5 == 0) {
-            System.out.println("RA");
-        } else if (number % 3 == 0) {
-            System.out.println("FIN");
-        } else {
-            System.out.println(number);
+        for(int i = 1; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FINRA ");
+            } else if (i % 5 == 0) {
+                System.out.print("RA ");
+            } else if (i% 3 == 0) {
+                System.out.print("FIN ");
+            } else {
+                System.out.print(i + " ");
 
+            }
         }
     }
 }
-
 /*
- For a number that is a multiple of 3, print "FIN" instead of the number.
-        For a number that is a multiple of 5, print "RA" instead of the number.
-        For a number that is a multiple of both 3 and 5, print "FINRA" instead of the number.
+Write a function that prints out the numbers from 1 to 30
+But for numbers that are a multiple of 3, print "FIN" instead of the number,
+and for numbers that are a multiple of 5, print "RA" instead of the number.
+For numbers that are a multiple of both 3 and 5, print "FINRA" instead of the number
+ */
 
-        Example:
-        number = 15
 
-        Output:
-        FINRA
-       */
 
 
 
