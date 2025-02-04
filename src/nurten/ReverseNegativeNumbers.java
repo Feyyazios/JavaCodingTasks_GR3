78c6ba6ef63e004827cca49af69de4b8a846bcdd
nurte <nurtenglryz@gmail.com>
2025-02-04 17:27:58 +0000

    Reverse negative number task has committed

---
+++ src/nurten/ReverseNegativeNumbers.java
@@ -0,0 +1,31 @@
+package nurten;
+
+public class ReverseNegativeNumbers {
+
+    /* **
+    Reverse digits of a negative number and return it as int input:-35 output:-53
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println(reverseNegativeNumbers(-35)); //output:-53
+    }
+
+
+
+    public static int reverseNegativeNumbers(int num) {
+
+        if (num >=0){  // If the number is positive, return as is
+            return num;
+        }
+
+
+        int reversed = 0;
+
+        while (num < 0) {
+            reversed = reversed * 10 + (num % 10); //Append the last digit
+            num /= 10; //Remove the last digit
+        }
+        return reversed; //return as negative
+    }
+}
